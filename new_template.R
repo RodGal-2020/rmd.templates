b24ab1020c08d33ca26f1d13f75994f6cbe3c5f1
José A. Rodríguez Gallego <96689554+RodGal-2020@users.noreply.github.com>
2023-08-04 12:57:29 +0200

    Feat - Initial templates

---
+++ new_template.R
@@ -0,0 +1,8 @@
+## Package manipulation
+devtools::document()
+devtools::install()
+
+## Adding templates
+# library(usethis)
+# new_template = "report_html"
+# use_rmarkdown_template(template_name = new_template)
